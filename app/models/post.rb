45f1cebc016ac131acd82c16f6821327cefe5f46
Yang <yang@truman.edu>
2017-09-11 16:11:16 -0400

    introduce banner_image_url

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,15 @@ class Post < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  # sort by descending order by the id
+  scope :most_recent, -> { order(id: :desc)}
+
   # change slug when the title is updated
   def should_generate_new_friendly_id?
     title_changed?
+  end
+
+  def display_day_published
+    "Published on #{created_at.strftime('%-b %-d, %-Y')}"
   end
 end
