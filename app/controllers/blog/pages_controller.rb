af2322d514ee452879d22fe27035ecc85701bd83
Yang <yang@truman.edu>
2017-09-11 17:06:50 -0400

    refactor pages and posts into blog

---
+++ app/controllers/blog/pages_controller.rb
@@ -0,0 +1,9 @@
+module Blog
+  class PagesController < ApplicationController
+    def about
+    end
+
+    def contact
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'about' => "pages#about", as: :about
-  get 'contact' => "pages#contact", as: :contact
+  get 'about' => "blog/pages#about", as: :about
+  get 'contact' => "blog/pages#contact", as: :contact
 
-  root to: "posts#index"
+  root to: "blog/posts#index"
 
-  resources :posts
+  scope module: 'blog' do
+    resources :posts
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
