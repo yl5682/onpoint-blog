80bc6c1681563531bba9d949344c63b693fb9c29
Yang <yang@truman.edu>
2017-09-11 10:33:57 -0400

    configure friendly for post and define root route

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  # change slug when the title is updated
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
 end
